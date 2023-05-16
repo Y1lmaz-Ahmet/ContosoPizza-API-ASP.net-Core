f69d273f5648a33db79c192dff9c0b9333c5effe
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-16 13:14:44 +0200

    pizza controller methode aanmaken en testen

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ContosoPizza_API_ASP.net_Core.Models;
+using ContosoPizza_API_ASP.net_Core.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoPizza_API_ASP.net_Core.Controllers
@@ -10,8 +12,18 @@ namespace ContosoPizza_API_ASP.net_Core.Controllers
         public PizzaController() { }
 
         // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
         // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if(pizza == null)
+                return NotFound();
+            return pizza;
+        }
 
         // POST action
 
