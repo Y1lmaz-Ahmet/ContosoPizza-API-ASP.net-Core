f9db62984ae9525f363c81992acd0ba2f2e7e495
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-16 13:09:38 +0200

    Leeg PizzaController aanmaken.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza_API_ASP.net_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController() { }
+
+        // GET all action
+
+        // GET by Id action
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
